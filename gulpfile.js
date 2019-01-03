7cfd671a6eeef3161bf8b888a11af33b102eca19
Hansin Susatya <hansinsusatya@gmail.com>
2019-01-03 16:40:02 +0700

    Rename app into docs

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,11 @@ var gulp = require('gulp'),
 	del = require('del');
 
 gulp.task('clean:js', () => {
-	return del('./app/dist/js');
+	return del('./docs/dist/js');
 });
 
 gulp.task('clean:css', () => {
-	return del('./app/dist/css');
+	return del('./docs/dist/css');
 });
 
 gulp.task('less', [ 'clean:css' ], async () => {
@@ -22,7 +22,7 @@ gulp.task('less', [ 'clean:css' ], async () => {
 		.pipe(less())
 		.pipe(cleanCSS())
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./app/dist/css'))
+		.pipe(gulp.dest('./docs/dist/css'))
 		.pipe(
 			browserSync.reload({
 				stream: true
@@ -32,10 +32,10 @@ gulp.task('less', [ 'clean:css' ], async () => {
 
 gulp.task('build:js', [ 'clean:js' ], () => {
 	return gulp
-		.src([ './node_modules/jquery/dist/jquery.min.js', './app/assets/js/index.js' ])
+		.src([ './node_modules/jquery/dist/jquery.min.js', './docs/assets/js/index.js' ])
 		.pipe(concat('tictactoe.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('./app/dist/js'))
+		.pipe(gulp.dest('./docs/dist/js'))
 		.pipe(
 			browserSync.reload({
 				stream: true
@@ -53,6 +53,6 @@ gulp.task('serve', () => {
 
 gulp.task('watch', [ 'less', 'build:js', 'serve' ], () => {
 	gulp.watch('./styles/**/**/*.less', [ 'less' ]);
-	gulp.watch('./app/*.html', browserSync.reload);
-	gulp.watch('./app/assets/js/**/*.js', [ 'build:js' ]);
+	gulp.watch('./docs/*.html', browserSync.reload);
+	gulp.watch('./docs/assets/js/**/*.js', [ 'build:js' ]);
 });
